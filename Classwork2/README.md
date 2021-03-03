4977062afb630ebb32b689244992d5847f19fde8
Paul Guszak <36202176+PGuszak@users.noreply.github.com>
2021-03-02 19:45:51 -0700

    fixed highlight and error JS - Complete

--- Classwork2/README.md
+++ Classwork2/README.md
@@ -2,6 +2,4 @@
 In Class 2 - using JS to validate the whole form.
 
 ##
-For this assingment all the js is correct as well as HTML.  But there is an unknown error that is keeping the form validate from working correctly 
-with highlighting and the txtcountry check. But this seems to be a error that only is affecting my code individually but aside from that is is 100% completed and implemented.
-
+For this assingment all the js is correct.  Highlights and error messages are displayed when called per the JS code.
--- Classwork2/js/script.js
+++ Classwork2/js/script.js
@@ -15,58 +15,64 @@ function updateCountryView(){
 
 function validateForm() {
 
-	var DoB = document.querySelector("#txtDOB");
-	var divDoBError = document.querySelector("#divDoBError");
-	var formIsValid = true;
-	if (DoB.value == "") {
-		divDoBError.classList.remove("invisible");
-		divDoBError.innerHTML = "The Date of Birth cannot be empty.";
-		DoB.classList.add("hasError");
-		formIsValid = false;
-	} else {
-		var DoBDate = new Date(DoB.value);
-		var todayDate = new Date();
-		if (DoBDate >= todayDate) {
-			divDoBError.classList.remove("invisible");
-			divDoBError.innerHTML = "The Date of Birth must be before today's date.";
-			DoB.classList.add("hasError");
-			formIsValid = false;
-		} else {
-			divDoBError.classList.add("invisible");
-			divDoBError.innerHTML = "";
-			DoB.classList.remove("hasError");
-			formIsValid = true;
-		}
-	}
+    var DoB = document.getElementById("txtDOB"); //why doesn't qeuery selector work????
+    var divDoBError = document.getElementById("divDoBError")
+    var formIsValid = true;
+    if (DoB.value == "") {
+        divDoBError.classList.remove("invisible");
+        divDoBError.innerHTML = "The Date of birth can not be empty."
+        DoB.classList.add("hasError");    
+        formIsValid = false;
+    }
+    else {
+        var DoBDate = new Date(DoB.value);
+        var todayDate = new Date();
+        if(DoBDate >= todayDate) {
+            divDoBError.classList.remove("invisible");
+            divDoBError.innerHTML = "Date of birth must be before today's date."
+            DoB.classList.add("hasError");    
+            formIsValid = false;
+                
+        }
+        else {
+            divDoBError.classList.add("invisible");
+            divDoBError.innerHTML = ""
+            DoB.classList.remove("hasError");    
+            }
+    
+    }
 
-	var ddCountry = document.querySelector("#ddCountry");
-	if (ddCountry.value == "Yes") {
-		var txtCountry = document.querySelector("#txtCountry");
-		if (txtCountry.value == "") {
-			document.querySelector("#divCountryError").classList.remove("invisible");
-			document.querySelector("#divCountryError").innerHTML = "Please enter at least one country.";
-			txtCountry.classList.add("hasError");
-			formIsValid = false;
-		} else {
-			document.querySelector("#divCountryError").classList.add("invisible");
-			document.querySelector("#divCountryError").innerHTML = "";
-			txtCountry.classList.add("hasError");
-			formIsValid = false;
-		}
-	}
+    //check 2
+    var ddCountry = document.getElementById("ddCountry");//why doesn't qeuery selector work????
+    if (ddCountry.value == "Yes") {
+        var inputtxtCountry = document.querySelector("#inputtxtCountry");
+        var countryError = document.getElementById("divCountryError");
+        if (inputtxtCountry.value == "") {
+            inputtxtCountry.classList.add("hasError");
+            countryError.classList.remove("invisible");
+            countryError.innerHTML = "Please enter the countries you have visited"
+            formIsValid = false;
+        }
+        else {
+            inputtxtCountry.classList.remove("hasError");
+            countryError.classList.add("invisible");
+            
+        }
 
-	var elements = document.getElementsByTagName("input");
-	var invalidChars = ['&', '<', '>', '#', '!', '"', '`', '~'];
-	for (let i = 0; i < elements.length; i++) {
-		for (let j = 0; j < invalidChars.length; j++) {
-			if (elements[i].value.indexOf(invalidChars[j]) != -1) {
-				elements[i].classList.add("hasError");
-				formIsValid = false;
-			}
-		}
-	}
+    }
+
+    var elements = document.getElementsByTagName("input")
+    var invalidChars = ['#', '!', '~']; //add all of them
+    for (let i = 0; i < elements.length; i++) {
+        for (let j = 0; j < invalidChars.length; j++) {
+            if (elements[i].value.indexOf(invalidChars[j]) != -1) {
+                elements[i].classList.add("hasError");
+                formIsValid = false;
+            }
+
+        }
+    }
 
 
-
-	return formIsValid;
+    return formIsValid;
 }
